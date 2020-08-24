9bde322e93b48ec22acb161ef90de571e3032cc5
Gleb94 <58596253+Gleb94@users.noreply.github.com>
2020-08-24 13:15:57 +0300

    Add files via upload

---
+++ Анкета/Objekt.js
@@ -0,0 +1,24 @@
+var myCar = new Object;
+myCar.make = "Chevrolet";
+myCar.model = "Comaro";
+myCar.year = 2019;
+
+var myDog = new Object();
+myDog.breed = "Labrador";
+myCar.name = "Dante";
+myCar.year = 5;
+
+var myPC = new Object();
+myPC.make = "Apple";
+myPC.model = "MacBook";
+myPC.year = 2020;
+
+var myCar = new Object();
+myCar.make = "Toyta";
+myCar.model = "Camry";
+myCar.year = 2017;
+
+var myCar = new Object();
+myCar.make = "BMW";
+myCar.model = "Mini Cooper";
+myCar.year = 2018;
